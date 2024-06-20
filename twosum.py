814e7d42903d69dc05909022d55a3b70562a8e3b
Kellik Simons <ksimons@aggies.ncat.edu>
2024-06-19 22:26:50 -0400

    First commit on twosum problem.

---
+++ twosum.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+
+
+test = Solution()
+
+print(test.twoSum([1,2,3,4], 6 )) 
